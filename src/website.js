ef05608f9f355844bf9f710cb9322cbe672eada0
shankarab <shankarab@gmail.com>
2016-07-11 16:47:56 -0700

    creating views dir to store html files

--- src/website.js
+++ src/website.js
@@ -4,10 +4,10 @@ var path = require("path");
 var app = express();
 
 app.get("/", function(request, response) {
-    response.sendFile(path.join("C:/Users/Tejas/Desktop/Node/login.html"));
+    response.sendFile(path.join(__dirname + "/login.html"));
 });
 app.get("/register", function(request, response) {
-    response.sendFile(path.join("C:/Users/Tejas/Desktop/Node/register.html"));
+    response.sendFile(path.join(___dirname + "/register.html"));
 });
 app.listen(8080);
 console.log("Running on port 8080");
