d60afbb04522304509528f550bb48c6e4bc24a0c
tejaskb04 <tejaskb@gmail.com>
2016-07-11 09:51:38 -0700

    Clicking button works

--- website.js
+++ website.js
@@ -1,9 +1,13 @@
 var express = require("express");
-var app = express();
+var url = require("url");
 var path = require("path");
+var app = express();
 
 app.get("/", function(request, response) {
     response.sendFile(path.join("C:/Users/Tejas/Desktop/Node/login.html"));
+});
+app.get("/register", function(request, response) {
+    response.sendFile(path.join("C:/Users/Tejas/Desktop/Node/register.html"));
 });
 app.listen(8080);
 console.log("Running on port 8080");
