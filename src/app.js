4a1568b7221cf242b0c9e42aec6eaed059e18221
shankarab <shankarab@gmail.com>
2016-07-12 02:12:12 -0700

    skeleton application using pug rendering engine

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// load express and other modules
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,14 +6,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+// set express to app
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +21,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routing - load router modules from routes directory, then mount them on to a path
+var routes = require('./routes/index');
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +56,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
-module.exports = app;
+app.listen(8080);
+console.log("Running on port 8080");
