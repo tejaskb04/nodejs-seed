c9789826796214fd659664cbf949b756c416c3dc
tejaskb04 <tejaskb@gmail.com>
2016-07-11 19:16:13 -0700

    moved stuff

--- src/website.js
+++ src/website.js
@@ -3,11 +3,14 @@ var url = require("url");
 var path = require("path");
 var app = express();
 
-app.get("/", function(request, response) {
-    response.sendFile(path.join(__dirname + "/login.html"));
+app.get("/login", function(request, response) {
+    response.sendFile(path.join(__dirname + "/views/login.html"));
 });
 app.get("/register", function(request, response) {
-    response.sendFile(path.join(___dirname + "/register.html"));
+    response.sendFile(path.join(__dirname + "/views/register.html"));
+});
+app.get("/", function(request, response) {
+    response.sendFile(path.join(__dirname + "/views/dashboard.html"));
 });
 app.listen(8080);
 console.log("Running on port 8080");
